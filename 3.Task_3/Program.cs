620bac97ff9a5b5e6907214f7da906610077925d
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-31 23:30:43 +0900

    Add files via upload

---
+++ 3.Task_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumberForArray = new Random();
+            Console.WriteLine("Please imput numbers of columsn");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[] testArray = new int[columns];
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                testArray[i] = randomNumberForArray.Next(0, 11);
+                Console.Write(testArray[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < (testArray.Length); i++)
+            {
+                if (i == 0 & testArray[i] > testArray[testArray.Length - testArray.Length+1])
+                {
+                    Console.Write(testArray[i] + " ");
+
+                }
+
+                if (i == testArray.Length - 1 & testArray[testArray.Length - 2] < testArray[i])
+                {
+                    Console.Write(testArray[i] + " ");
+                }
+
+                if (i != (testArray.Length - 1) & i != (testArray.Length - testArray.Length))
+                {
+                    if (testArray[i] > testArray[i - 1] & testArray[i] > testArray[(i + 1)])
+                    {
+                        Console.Write(testArray[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
