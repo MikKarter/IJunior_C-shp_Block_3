dc44ed1e2940be19e1562dbb0b767f9d2deb3705
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-31 23:40:40 +0900

    Update Program.cs

--- 3.Task_1/Program.cs
+++ 3.Task_1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,27 +10,45 @@ namespace _3.Task_1
     {
         static void Main(string[] args)
         {
-            Random randomNumberOne = new Random();
-            Random randomNumberTwo = new Random();
-            int[,] testArray = new int[randomNumberOne.Next(4,11), randomNumberTwo.Next(4,14)];
+            Random randomNumber = new Random();
+            int firstNumberForRandom = 4;
+            int secondNumberForRandom = 14;
+            int[,] testArray = new int[randomNumber.Next(firstNumberForRandom, secondNumberForRandom), randomNumber.Next(firstNumberForRandom, secondNumberForRandom)];
 
             int rowTestArray = 2;
             int columnTestArray = 1;
             int multiplicationNumbers = 1;
             int sumNumbers = 0;
 
+            Console.WriteLine("Введите число для нижней границы рандома");
+            firstNumberForRandom = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число для верхней границы рандома");
+            secondNumberForRandom = Convert.ToInt32(Console.ReadLine());
+
             for (int i = 0; i < testArray.GetLength(0); i++)
             {
                 for (int j = 0; j < testArray.GetLength(1); j++)
                 {
-                    testArray[i,j] = randomNumberOne.Next(50,77);
+                    testArray[i,j] = randomNumber.Next(firstNumberForRandom, secondNumberForRandom);
                     Console.Write(testArray[i, j] + " ");
-                    sumNumbers += testArray[rowTestArray, j];
                 }
 
-                multiplicationNumbers *= testArray[i, columnTestArray];
                 Console.WriteLine();
             }
+
+            for (int i = 0; i < testArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < testArray.GetLength(1); j++)
+                {
+                    sumNumbers += testArray[rowTestArray, j];
+                }
+            }
+
+            for (int i = 0; i < testArray.GetLength(0); i++)
+            {
+                multiplicationNumbers *= testArray[i, columnTestArray];
+            }          
+
             Console.WriteLine(multiplicationNumbers);
             Console.WriteLine(sumNumbers);
         }
