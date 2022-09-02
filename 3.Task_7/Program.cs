679749b981f10e9d8615d2626ff461a5d546d63c
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-02 16:23:03 +0900

    Add files via upload

---
+++ 3.Task_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            string[] words = phrase.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
